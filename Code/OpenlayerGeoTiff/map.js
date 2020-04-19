523d44f1ffdddec1fe9831a5d78ec2d015d360e3
ARiisgaard <44462329+ARiisgaard@users.noreply.github.com>
2020-04-19 19:41:58 +0200

    Better clarity
    Added documentation, beautified content and moved the tileMetadata-decipher to its own file for clarity

--- Code/OpenlayerGeoTiff/map.js
+++ Code/OpenlayerGeoTiff/map.js
@@ -1,189 +1,148 @@
-  const projection = ol.proj.get('EPSG:4326');
-  
-  
-  //This is the 
-  var tileMetadata = {};
-  //This is the folder, where the tiles are extracted from. Locally it can be "g2tTiles" for India or "newTestTiles" for the States
-  tileMetadata.tileFolder = 'g2tTiles'
+const projection = ol.proj.get('EPSG:4326');
 
-//Making a request for the tileData - see function below
-  function loadDoc() {
-      var xhttp = new XMLHttpRequest();
-      xhttp.open("GET", tileMetadata.tileFolder+"/tilemapresource.xml", false);
-      xhttp.send();
-      getTileMetadata(xhttp);//(this);
-  }
+////////////
+// Getting Metadata
+////////////
 
+//Get tile-metadata produced by gdal2tiles32 and store it in the tileMetadata object. Necessary to ensure correct resolutions among other things
+var tileMetadata = {};
 
-  //When gdal2tiles32.py is run it creates a xml file with metadata. This function extracts the relevant data
-  function getTileMetadata(xml)
-  {
-      var xmlDoc = xml.responseXML;
-      var parser = new DOMParser();
-      var xmlDoc = parser.parseFromString(xml.responseText, "application/xml");
-      
-      
-      //Getting the resolutions for the tiles. 
-      var numberOfZoomLevels = xmlDoc.getElementsByTagName("TileSets")[0].childElementCount;//.childNodes[0]
-      tileMetadata.resolutions = [];
-      tileMetadata.matrixIds = [];
-      for (z = 0; z < numberOfZoomLevels; z++) {
-        tileMetadata.resolutions[z] = parseFloat(xmlDoc.getElementsByTagName("TileSets")[0].children[z].attributes["units-per-pixel"].value);
-        tileMetadata.matrixIds[z] = parseFloat(xmlDoc.getElementsByTagName("TileSets")[0].children[z].attributes["order"].value);
+//The folder, where the tiles are extracted from. Locally it can be "g2tTiles" for India or "newTestTiles" for the States
+tileMetadata.tileFolder = 'g2tTiles';
+getTileMetadata(tileMetadata);
 
-      }
-      
-      
-      //Getting the coordinates for bounding box, origin and center
-      var minx = parseFloat(xmlDoc.getElementsByTagName("BoundingBox")[0].attributes.minx.value);
-      var maxx = parseFloat(xmlDoc.getElementsByTagName("BoundingBox")[0].attributes.maxx.value);
-      var miny = parseFloat(xmlDoc.getElementsByTagName("BoundingBox")[0].attributes.miny.value);
-      var maxy = parseFloat(xmlDoc.getElementsByTagName("BoundingBox")[0].attributes.maxy.value);
-      tileMetadata.boundingBox = [minx, miny, maxx, maxy]
-      tileMetadata.origin = [minx, maxy]
-      tileMetadata.center = [(minx+maxx)/2,(miny+maxy)/2]
-      
-  }
-loadDoc();
+//Directory with maxvalues for each tile - Used for coloring later
+var maxValue = {};
+$.getJSON("maxValues.json", function(json) {
+  maxValue = json
+});
+////////////
 
-// 
-var tileGrid = new ol.tilegrid.WMTS({
-  origin: tileMetadata.origin,
-  resolutions: tileMetadata.resolutions,
-  matrixIds: tileMetadata.matrixIds,
-  tileSize: 256,//[256, 256],
-})
+//Creating the layer with population data. Seperated into three variables, because they are called individually to apply the coloring
+var tileGrid = new ol.tilegrid.WMTS({origin: tileMetadata.origin, resolutions: tileMetadata.resolutions, matrixIds: tileMetadata.matrixIds, tileSize: 256})
 
 var tileSource = new ol.source.WMTS({
   // url: 'http://webportals.ipsl.jussieu.fr/ScientificApps/dev/forge_patrick/eox/tileSet/{TileMatrix}/{TileRow}/{TileCol}.tif',
-  url: tileMetadata.tileFolder+'/{TileMatrix}/{TileCol}/{TileRow}.tiff', //I have a folder with the testRaster choped up in 
+  url: tileMetadata.tileFolder + '/{TileMatrix}/{TileCol}/{TileRow}.tiff',
   projection: projection,
   tileGrid: tileGrid,
   requestEncoding: 'REST',
   transition: 0
 })
 
-//Opens the file with the max values for each tile
-var maxValue = {};
-$.getJSON("maxValues.json", function(json) {
-    maxValue = json
+var wmslayer = new ol.layer.Tile({
+  source: tileSource, extent: tileMetadata.boundingBox //The extent has been limited, since there I didn't test with the raster for the entire world
 });
-
-
-
-  var wmslayer = new ol.layer.Tile({
-    source: tileSource,
-    extent: tileMetadata.boundingBox
-  });
 
 // define the base layer
 var osmSource = new ol.source.OSM();
- var osm =	new ol.layer.Tile({
-      source: osmSource,
-      extent: tileMetadata.boundingBox
-    });
+var osm = new ol.layer.Tile({source: osmSource, extent: tileMetadata.boundingBox});
 
 // define the map
-  var map = new ol.Map({
-    target: 's2map',
-    layers: [
-osm,
-      wmslayer,
-    ],
-wrapDateLine: true,
-    view: new ol.View({
-      projection,
-      center: tileMetadata.center,
-      zoom: 7,
-      maxZoom: 11,
-      minZoom: 2
-    }),
-    controls: ol.control.defaults({
-      attributionOptions: {
-        collapsible: false
-      }
-    }),
-  });
+var map = new ol.Map({
+  target: 's2map',
+  layers: [
+    osm, wmslayer
+  ],
+  wrapDateLine: true,
+  view: new ol.View({projection, center: tileMetadata.center, zoom: 7, maxZoom: 11, minZoom: 2}),
+  controls: ol.control.defaults({
+    attributionOptions: {
+      collapsible: false
+    }
+  })
+});
 
 // olGeoTiff setup
-  var olgt_map = new olGeoTiff(wmslayer);
-  olgt_map.plotOptions.domain = [0, 2000];
-  olgt_map.plotOptions.noDataValue = -9999;
-  olgt_map.plotOptions.palette = 'rainbow';
+var olgt_map = new olGeoTiff(wmslayer);
+olgt_map.plotOptions.domain = [0, 2000];
+olgt_map.plotOptions.noDataValue = -9999;
+olgt_map.plotOptions.palette = 'rainbow';
 
 // handle user input
 $(window).on('load', function() {
-    
-  
-    var $container = $('#s2map').parent();
 
-    // slider1 (domain)
-      var slider = $container.find('.domainslider')[0];
+  var $container = $('#s2map').parent();
 
-      noUiSlider.create(slider, {
-        start: olgt_map.plotOptions.domain,
-        connect: true,
-        range: { 'min': 0, 'max': 15000 }, // TODO: Calculate these based on the current extent 
-        tooltips: true,
-      });
+  // slider1 (domain)
+  var slider = $container.find('.domainslider')[0];
 
-      slider.noUiSlider.on('change', function(values) {
-        olgt_map.plotOptions.domain = [values[0], values[1]];
-        olgt_map.redraw();
-      });
+  noUiSlider.create(slider, {
+    start: olgt_map.plotOptions.domain,
+    connect: true,
+    range: {
+      'min': 0,
+      'max': 15000
+    }, // TODO: Calculate these based on the current extent
+    tooltips: true
+  });
 
-    // slider2 (opacity)
-      var slider2 = $container.find('.opacityslider')[0];
+  slider.noUiSlider.on('change', function(values) {
+    olgt_map.plotOptions.domain = [
+      values[0], values[1]
+    ];
+    olgt_map.redraw();
+  });
 
-      var opacity = 0.5;
-      noUiSlider.create(slider2, {
-        start: opacity,
-        connect: true,
-        range: { 'min': 0, 'max': 1 },
-        tooltips: true,
-      });
-      wmslayer.setOpacity(opacity);
+  // slider2 (opacity)
+  var slider2 = $container.find('.opacityslider')[0];
 
-      slider2.noUiSlider.on('slide', function(values) {
-        wmslayer.setOpacity(values[0]*1);
-      });
+  var opacity = 0.5;
+  noUiSlider.create(slider2, {
+    start: opacity,
+    connect: true,
+    range: {
+      'min': 0,
+      'max': 1
+    },
+    tooltips: true
+  });
+  wmslayer.setOpacity(opacity);
 
-    // palette
-      $container.find('.palette').on("change", function() {
-        var palette = this.options[this.selectedIndex].text;
-        olgt_map.plotOptions.palette = palette;
-        olgt_map.redraw();
-      });
+  slider2.noUiSlider.on('slide', function(values) {
+    wmslayer.setOpacity(values[0] * 1);
+  });
 
-console.log(maxValue)
-map.on("moveend", function() {
+  // palette
+  $container.find('.palette').on("change", function() {
+    var palette = this.options[this.selectedIndex].text;
+    olgt_map.plotOptions.palette = palette;
+    olgt_map.redraw();
+  });
 
-        var zoom = map.getView().getZoom(); //originally this was -1??
-        var zoomInfo = 'Zoom level = ' + zoom;
-        document.getElementById('zoomlevel').innerHTML = zoomInfo;
+  //Recolor map on movement or zoom
+  map.on("moveend", function() {
 
-        var mapExtent = map.getView().calculateExtent(map.getSize())
-        var mapZoom = map.getView().getZoom();
-        var tileUrlFunction = tileSource.getTileUrlFunction()
-        var currentMax = 0;
-        tileGrid.forEachTileCoord(mapExtent, mapZoom, function (tileCoord) {
-        
-          tileName = tileUrlFunction(tileCoord, ol.proj.get('EPSG:4326'))
-          
-          if (maxValue[tileName] !== undefined && maxValue[tileName] > currentMax){
-            currentMax = maxValue[tileName]
-          }
-          
-          // console.log(maxValue[tileName])
-          console.log({currentMax})
-          // console.log(maxValue[tileName])
-          
-        
-        })
-        olgt_map.plotOptions.domain = [0, currentMax];
-        olgt_map.redraw();
-        
-    });
+    var mapExtent = map.getView().calculateExtent(map.getSize())
+    var mapZoom = map.getView().getZoom();
+
+    //Temporary: To keep track of which zoom layers is being displayed
+    var zoomInfo = 'Zoom level = ' + mapZoom;
+    document.getElementById('zoomlevel').innerHTML = zoomInfo;
+
+    //Calculating the max population - Default is set to 0
+    var currentMax = 0;
+
+    //Function for getting the url/filename for tiles based on their coordinates
+    var tileUrlFunction = tileSource.getTileUrlFunction()
+
+    //Checks which tiles that currently are being displayed
+    tileGrid.forEachTileCoord(mapExtent, mapZoom, function(tileCoord) {
+
+      //Gets the name of each currently displayed tile
+      tileName = tileUrlFunction(tileCoord, ol.proj.get('EPSG:4326'))
+
+      //Gets the highest value in each tile by looking in the maxValue-dictionary. If a new highest value is found it is saved in currentMax
+      if (maxValue[tileName] !== undefined && maxValue[tileName] > currentMax) {
+        currentMax = maxValue[tileName]
+      }
+
+    })
+
+    //Recolors the map based on the highest value found
+    olgt_map.plotOptions.domain = [0, currentMax];
+    olgt_map.redraw();
 
   });
 
+});
--- Code/TileMaxValue.py
+++ Code/TileMaxValue.py
@@ -1,8 +1,12 @@
+#This script creates a json-file with names and max values for each tiff-tile
+
 import gdal
 import numpy as np
 import os
-tileFolder = "g2tTiles"
+
+#Directory containing the maptiles and the path to it
 tilePath = os.getcwd() + "\\OpenlayerGeotiff\\"
+tileFolder = "g2tTiles"
 
 #Function for getting values out of the first band of a tiff file
 def openTIFFasNParray(file):
@@ -15,44 +19,41 @@ def openTIFFasNParray(file):
     return a
 
 
-
-#This function creates a 
-
+#maxValue is going to contain the content of the json file before it gets written to the file. 
+#It is initially defined as "{", because jsons start with this
 maxValue = "{"
+
+#Goes through each tiff and find their highest value. The filename and highest value then gets added to the string
 zoomlevel = [ f.name for f in os.scandir(tilePath + tileFolder) if f.is_dir() ]
 for z in zoomlevel:
     TileCol = [ f.name for f in os.scandir(tilePath + tileFolder + "\\"+z) if f.is_dir() ]
     for c in TileCol:
         TileRow = [ f.name for f in os.scandir(tilePath + tileFolder + "\\"+z+ "\\" +c) if ".tiff" in f.name]
         for r in TileRow:
+            #The name used by python to locate the file
             TileName = tileFolder  + "\\" + z + "\\" + c + "\\" + r
-                        # print(tilePath + TileName)
+
+            #Opens the file and finds it highest value
             pop = openTIFFasNParray(tilePath + TileName)
             maxPop = 0
             if(np.max(pop) > maxPop):
                 maxPop = np.max(pop)
             
-            NameWithDoubleEscape = tileFolder  + "/" + z + "/" + c + "/" + r #Double \\ are needed for the final result, since a spare backslash is needed for escaping the backslash in js
-            maxValue = maxValue + "\"" + NameWithDoubleEscape + "\":" + str(maxPop) + ","
+            #Name used by javascript. It is a different format, because it needs to match the formate of the forEachTileCoord-function
+            TileNameJS = tileFolder  + "/" + z + "/" + c + "/" + r 
+            
+            #Appends the filename and max value to the json-string
+            maxValue = maxValue + "\"" + TileNameJS + "\":" + str(maxPop) + ","
 
-maxValue = maxValue[:-1]#Removed the trailing ,            
+#To finish the json the trailing comma is remove a "}" is placed in the end  
+maxValue = maxValue[:-1]          
 maxValue = maxValue + "}"           
             
             
-        
+#The json-string gets written to a file        
 print(maxValue)
 file = open(tilePath + "maxValues.json", "w+")        
 file.write(maxValue)
 file.close()  
 
 print("Writting done")
-
-
-    
-# 
-# for year in range(2010,2101,10):
-# 
-#     pop = openTIFFasNParray(datadir + model + '\\' + ssp + '\\popmean-' + str(year) + '-clipped.tiff')
-# 
-#     if(np.max(pop) > maxPop):
-#         maxPop = np.max(pop)
