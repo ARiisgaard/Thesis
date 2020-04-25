a9e9937d35880c7b56bc6caf93753152bacccf13
ARiisgaard <44462329+ARiisgaard@users.noreply.github.com>
2020-04-25 17:18:21 +0200

    Now everything is broken in the same way

--- Code/OpenlayerGeoTiff/map.js
+++ Code/OpenlayerGeoTiff/map.js
@@ -15,6 +15,9 @@ getTileMetadata(tileMetadata);
 var maxValue = {};
 $.getJSON("maxValues.json", function(json) {
   maxValue = json
+  console.log({maxValue})
+  //Color the map based on the loaded data  
+  // recolorMap()
 });
 ////////////
 
@@ -36,7 +39,9 @@ var wmslayer = new ol.layer.Tile({
 
 // define the base layer
 var osmSource = new ol.source.OSM();
-var osm = new ol.layer.Tile({source: osmSource, extent: tileMetadata.boundingBox});
+var osm = new ol.layer.Tile({source: osmSource
+  // , extent: tileMetadata.boundingBox
+});
 
 // define the map
 var map = new ol.Map({
@@ -53,41 +58,36 @@ var map = new ol.Map({
   })
 });
 
+//Creation of colorscale
+
+var colorScale = {};
+colorScale.color_steps = ['#fef0d9','#fdd49e','#fdbb84','#fc8d59','#e34a33','#b30000']
+colorScale.percentage_steps = [0, 0.2, 0.4, 0.6, 0.8, 1]
+plotty.addColorScale("sequentialMultiHue6Colors", colorScale.color_steps, colorScale.percentage_steps);
+
 // olGeoTiff setup
 var olgt_map = new olGeoTiff(wmslayer);
 olgt_map.plotOptions.domain = [0, 2000];
 olgt_map.plotOptions.noDataValue = -9999;
-olgt_map.plotOptions.palette = 'rainbow';
+olgt_map.plotOptions.palette = 'sequentialMultiHue6Colors';
+
+
 
 // handle user input
 $(window).on('load', function() {
 
+
+//Add the colors from the color palette to the legend
+  for (i = 0; i < colorScale.percentage_steps.length; i++) {
+    document.getElementById('d'+String(i)).style.background = colorScale.color_steps[i]
+}
+
   var $container = $('#s2map').parent();
-
-  // slider1 (domain)
-  var slider = $container.find('.domainslider')[0];
-
-  noUiSlider.create(slider, {
-    start: olgt_map.plotOptions.domain,
-    connect: true,
-    range: {
-      'min': 0,
-      'max': 15000
-    }, // TODO: Calculate these based on the current extent
-    tooltips: true
-  });
-
-  slider.noUiSlider.on('change', function(values) {
-    olgt_map.plotOptions.domain = [
-      values[0], values[1]
-    ];
-    olgt_map.redraw();
-  });
 
   // slider2 (opacity)
   var slider2 = $container.find('.opacityslider')[0];
 
-  var opacity = 0.5;
+  var opacity = 0.7;
   noUiSlider.create(slider2, {
     start: opacity,
     connect: true,
@@ -102,47 +102,61 @@ $(window).on('load', function() {
   slider2.noUiSlider.on('slide', function(values) {
     wmslayer.setOpacity(values[0] * 1);
   });
-
-  // palette
-  $container.find('.palette').on("change", function() {
-    var palette = this.options[this.selectedIndex].text;
-    olgt_map.plotOptions.palette = palette;
-    olgt_map.redraw();
-  });
+  var Uppervalue = 0; 
 
   //Recolor map on movement or zoom
   map.on("moveend", function() {
-
-    var mapExtent = map.getView().calculateExtent(map.getSize())
-    var mapZoom = map.getView().getZoom();
-
-    //Temporary: To keep track of which zoom layers is being displayed
-    var zoomInfo = 'Zoom level = ' + mapZoom;
-    document.getElementById('zoomlevel').innerHTML = zoomInfo;
-
-    //Calculating the max population - Default is set to 0
-    var currentMax = 0;
-
-    //Function for getting the url/filename for tiles based on their coordinates
-    var tileUrlFunction = tileSource.getTileUrlFunction()
-
-    //Checks which tiles that currently are being displayed
-    tileGrid.forEachTileCoord(mapExtent, mapZoom, function(tileCoord) {
-
-      //Gets the name of each currently displayed tile
-      tileName = tileUrlFunction(tileCoord, ol.proj.get('EPSG:4326'))
-
-      //Gets the highest value in each tile by looking in the maxValue-dictionary. If a new highest value is found it is saved in currentMax
-      if (maxValue[tileName] !== undefined && maxValue[tileName] > currentMax) {
-        currentMax = maxValue[tileName]
-      }
-
-    })
-
-    //Recolors the map based on the highest value found
-    olgt_map.plotOptions.domain = [0, currentMax];
-    olgt_map.redraw();
-
+     recolorMap()
   });
+  
+
 
 });
+
+// Find the highest value currently displayed and recolor based on this
+function recolorMap(){
+
+  
+  
+    var mapExtent = map.getView().calculateExtent(map.getSize())
+    var mapZoom = map.getView().getZoom()  - 7;
+    // var mapZoom = 0
+    console.log(mapZoom)
+    var testing = false //Delete later
+    //Calculating the max population - Default is set to 0
+    var currentMax = 0;
+  
+    //Function for getting the url/filename for tiles based on their coordinates
+    var tileUrlFunction = tileSource.getTileUrlFunction()
+    var currentTiles = [];
+    //Checks which tiles that currently are being displayed
+    tileSource.tileGrid.forEachTileCoord(mapExtent, mapZoom, function(tileCoord) {
+  
+      //Gets the name of each currently displayed tile
+      tileName = tileUrlFunction(tileCoord, ol.proj.get('EPSG:4326'))
+      currentTiles.push(tileName);
+      
+      //Delete later
+      if (testing == false) {
+        console.log({tileName})
+        testing = true
+      }
+      
+      
+      //Gets the highest value in each tile by looking in the maxValue-dictionary. If a new highest value is found it is saved in currentMax
+      // if (maxValue[tileName] !== undefined && maxValue[tileName] > currentMax) {
+      //   currentMax = maxValue[tileName]
+      //   console.log("Goes here?")
+      // }
+  
+    })
+  
+    //Recolors the map based on the highest value found
+    // olgt_map.plotOptions.domain = [0, currentMax];
+    olgt_map.redraw(olgt_map, currentTiles, colorScale);
+    
+  
+    //Updates the legend
+
+
+}
